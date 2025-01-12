28a2ce1dc7eafd3a63bc4dd00f28eaf162d068e2
dylsteck <dylancsteck@gmail.com>
2025-01-12 12:16:57 -0500

    added more days

--- lib/data.ts
+++ lib/data.ts
@@ -4,7 +4,7 @@ export const DAY_CONTENT: DayContent[] = [
     {
       date: new Date('2025-01-11T00:00:00-05:00'),
       sandwich: [
-        { id: "1", imageUrl: "/sandwiches/utopia-bec-jan-11-25.png", description: "Bacon egg and cheese with a hash brown on an everything bagel" }
+        { id: "1", imageUrl: "/sandwiches/utopia-bec-jan-11-25.png", description: "bacon egg and cheese with a hash brown on an everything bagel" }
       ],
       rating: [
         { rating: 4, description: "perfect combination and the hash brown was so good" }
@@ -14,15 +14,123 @@ export const DAY_CONTENT: DayContent[] = [
       ]
     },
     {
-        date: new Date('2025-12-08T00:00:00-05:00'),
+        date: new Date('2024-12-08T00:00:00-05:00'),
         sandwich: [
-          { id: "1", imageUrl: "/sandwiches/grabsteins-bec-dec-8-24.png", description: "Bacon egg and cheese with a hash brown on an everything bagel" }
+          { id: "1", imageUrl: "/sandwiches/grabsteins-bec-dec-8-24.png", description: "bacon egg and cheese with a hash brown on an everything bagel" }
         ],
         rating: [
           { rating: 4, description: "one of my go-to favorites lately" }
         ],
         location: [
           { name: "grabsteins bagels", url: "https://grabsteinsbagels.square.site" }
+        ]
+    },
+    {
+        date: new Date('2024-12-07T00:00:00-05:00'),
+        sandwich: [
+          { id: "1", imageUrl: "/sandwiches/hh-bec-dec-7-24.png", description: "bacon egg and cheese on an everything bagel" }
+        ],
+        rating: [
+          { rating: 2, description: "not that good tbh" }
+        ],
+        location: [
+          { name: "h&h bagels", url: "https://www.hhbagels.com" }
+        ]
+    },
+    {
+        date: new Date('2024-11-17T00:00:00-05:00'),
+        sandwich: [
+          { id: "1", imageUrl: "/sandwiches/tompkins-squre-bec-nov-17-24.png", description: "bacon egg and cheese with avocado on an everything bagel" }
+        ],
+        rating: [
+          { rating: 3, description: "quite good as always but a but messy with the avocado" }
+        ],
+        location: [
+          { name: "tompkins square bagels", url: "https://www.tompkinssquarebagels.com" }
+        ]
+    },
+    {
+        date: new Date('2024-11-09T00:00:00-05:00'),
+        sandwich: [
+          { id: "1", imageUrl: "/sandwiches/sipnsoda-bec-nov-9-24.png", description: "bacon egg and cheese with a hash brown on a roll" }
+        ],
+        rating: [
+          { rating: 4, description: "everything about this was perfect, especially the crunch from the bacon and hash brown" }
+        ],
+        location: [
+          { name: "sip'n soda", url: "https://www.sipnsoda.com" }
+        ]
+    },
+    {
+        date: new Date('2024-10-20T00:00:00-05:00'),
+        sandwich: [
+          { id: "1", imageUrl: "/sandwiches/zuckers-bec-10-20-24.png", description: "bacon egg and cheese with a latke on an everything bagel" }
+        ],
+        rating: [
+          { rating: 4, description: "very good and the latke is a must, but the egg could've been a little better" }
+        ],
+        location: [
+          { name: "zucker's bagels", url: "https://www.zuckersbagels.com" }
+        ]
+    },
+    {
+        date: new Date('2024-09-27T00:00:00-05:00'),
+        sandwich: [
+          { id: "1", imageUrl: "/sandwiches/bec-eggslut-sept-27-24.png", description: "bacon egg and cheese on a brioche bun with truffle hash browns" }
+        ],
+        rating: [
+          { rating: 4, description: "a near perfect meal" }
+        ],
+        location: [
+          { name: "eggslut", url: "https://www.eggslut.com" }
+        ]
+    },
+    {
+        date: new Date('2024-09-23T00:00:00-05:00'),
+        sandwich: [
+          { id: "1", imageUrl: "/sandwiches/roses-bec-sept-23-24.png", description: "bacon egg and cheese with brocoli rabe on focaccia" }
+        ],
+        rating: [
+          { rating: 3, description: "very messy and had a bit too much on it" }
+        ],
+        location: [
+          { name: "rose's cafe", url: "https://rosescafesf.com" }
+        ]
+    },
+    {
+        date: new Date('2024-09-16T00:00:00-05:00'),
+        sandwich: [
+          { id: "1", imageUrl: "/sandwiches/utopia-sec-sept-16-24.png", description: "sausage egg and cheese with avocado on an everything bagel" }
+        ],
+        rating: [
+          { rating: 4, description: "quite good but i should've gotten bacon instead" }
+        ],
+        location: [
+          { name: "utopia bagels", url: "https://www.utopiabagelsny.com" }
+        ]
+    },
+    {
+        date: new Date('2024-09-06T00:00:00-05:00'),
+        sandwich: [
+          { id: "1", imageUrl: "/sandwiches/winson-bec-sept-6-24.png", description: "bacon egg and cheese on a scallion pancake" }
+        ],
+        rating: [
+          { rating: 4, description: "amazing especially with the sauce but very greasy and not something you can eat all the time" }
+        ],
+        location: [
+          { name: "win son bakery", url: "https://winsonbrooklyn.com/bakery" }
+        ]
+    },
+    {
+        date: new Date('2024-07-31T00:00:00-05:00'),
+        sandwich: [
+          { id: "1", imageUrl: "/sandwiches/wexlers-bec-july-31-24.png", description: "bacon egg and cheese on a croissant with a side hash brown" }
+        ],
+        rating: [
+          { rating: 4, description: "so good that it made me want to have more egg sandwiches on croissants" }
+        ],
+        location: [
+          { name: "wexler's deli", url: "https://www.wexlersdeli.com" }
         ]
     },
 ]
